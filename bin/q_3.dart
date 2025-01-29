18bf71939536351e1f3aa3bd687a73a0eeb69379
maryd <mary.digila@chillarcards.com>
2025-01-29 12:34:14 +0530

    updated

--- bin/q_3.dart
+++ bin/q_3.dart
@@ -2,7 +2,7 @@ class Shape {
   var _color = "white";
 
   displayColor() {
-    print(_color);
+    print("Color is initialized as : $_color");
   }
 
   get getColor {
@@ -51,22 +51,26 @@ void main() {
   //shape
   Shape objS = Shape();
   objS.displayColor();
-  print(objS.getColor);
-  print(objS.setColor = "Red");
+  objS.setColor = "Red";
+  print("Color has been changed and set to : ${objS.getColor}");
 
 //circle
   Circle objC = Circle(4);
-  print(objC.getRad);
+  print("Circle radius initialized as :${objC.getRad}");
   objC.setRad = 9;
-  print(objC.getRad);
+  print("Circle radius has been changed and set to: ${objC.getRad}");
 
 //rectangle
   Rectangle objR = Rectangle(3, 4);
-  print(objR.getLen);
-  print(objR.getWid);
+  // objR.getLen;
+  // objR.getWid;
+  print(
+      "Rectangle length , width  is Initialized as: ${objR.getLen},${objR.getWid}");
 
   objR.setLen = 6;
   objR.setWid = 9;
-  print(objR.getLen);
-  print(objR.getWid);
+  // print(objR.getLen);
+  // print(objR.getWid);
+  print(
+      "Rectangle length and width is changed and set to ${objR.getLen},${objR.getWid}");
 }
